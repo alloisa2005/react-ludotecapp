fe55fc3c3c4154c913f5f061fd3e5fdeeffe3d24
Anthony Allois <anthonyallois@gmail.com>
2022-07-01 14:02:14 -0300

    Mas ajustes

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,38 +1,37 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom';
-import { CartContext } from '../../context/CartContext';
-import CartDetail from '../CartDetail/CartDetail';
-import './CartContainer.css'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../../context/CartContext";
+import CartDetail from "../CartDetail/CartDetail";
+import "./CartContainer.css";
 
 function CartContainer() {
-
   const [cartList, setCartList, cantidadItems] = useContext(CartContext);
 
   return (
     <>
-          <div className="cartContainer_title">
-            <h2>
-              Mi Carrito ({cantidadItems()}{" "}
-              {cantidadItems() > 1 ? "items" : "item"})
-            </h2>
-            <Link to={"/"} className="a_per">              
-              <h4>Seguir Comprando</h4>{" "}
-            </Link>
-          </div>
+      <div className="cartContainer_title">
+        <h2>
+          Mi Carrito ({cantidadItems()} {cantidadItems() > 1 ? "items" : "item"}
+          )
+        </h2>
+        <Link to={"/"} className="a_per">
+          <h4>Seguir Comprando</h4>{" "}
+        </Link>
+      </div>
 
-          <div className="cartDetail_container">
-            <div className="cartDetail_container_left">
-              {cartList.map((item) => (
-                <CartDetail item={item} />
-              ))}
-            </div>
+      <div className="cartDetail_container">
+        <div className="cartDetail_container_left">
+          {cartList.map((item) => (
+            <CartDetail key={item.id} item={item} />
+          ))}
+        </div>
 
-            <div className="cartDetail_container_right">
-              <h2>Resumen del Pedido</h2>
-            </div>
-          </div>
-        </>
-  )
+        <div className="cartDetail_container_right">
+          <h2>Resumen del Pedido</h2>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default CartContainer
+export default CartContainer;
--- src/components/CartDetail/CartDetail.js
+++ src/components/CartDetail/CartDetail.js
@@ -1,37 +1,59 @@
-
-import React, { useContext } from 'react'
-import { separadorMiles, tituloMayuscula } from '../../utilidades/Utilidades'
-import './CartDetail.css'
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import { CartContext } from '../../context/CartContext';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { separadorMiles, tituloMayuscula } from "../../utilidades/Utilidades";
+import "./CartDetail.css";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { CartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
 
 function CartDetail({ item }) {
-  const [cartList, setCartList, cantidadItems, clearCart, addCart, removeItem] = useContext(CartContext);
+  const [cartList, setCartList, cantidadItems, clearCart, addCart, removeItem] =
+    useContext(CartContext);
+
+  const sumarItem = () => {
+    // Añado items mientras haya en stock de ese producto
+    if (item.stock > item.quantity) {
+      addCart(item, 1);
+    }
+  };
+
+  const restarItem = () => {
+    if (item.quantity > 0) {
+      addCart(item, -1);
+    }
+  };
 
   return (
-    <div className='cart_item_detail'>
-        <div style={{ width:"25%"}}>
-            <Link to={`/item/${item.id}`} className="a_per">
-                <img src={item.img} alt={item.nombre} />
-            </Link>
+    <div className="cart_item_detail">
+      <div style={{ width: "25%" }}>
+        <Link to={`/item/${item.id}`} className="a_per">
+          <img src={item.img} alt={item.nombre} />
+        </Link>
+      </div>
+      <div className="cart_item_detail_text">
+        <h3>{item.nombre}</h3>
+        <h4>
+          Categoría: <span> {tituloMayuscula(item.categoria)} </span>
+        </h4>
+        <h4>
+          Precio Unitario: <span> $ {separadorMiles(item.precio)} </span>
+        </h4>
+        <div className="cart_item_detail_cant">
+          <div className="cant_detail">
+            <button onClick={restarItem}>-</button>
+            <p>{item.quantity}</p>
+            <button onClick={sumarItem}>+</button>
+          </div>
+          <DeleteOutlinedIcon
+            className="cant_detail_icon"
+            onClick={() => removeItem(item.id)}
+          />
         </div>
-        <div className="cart_item_detail_text">
-            <h3>{item.nombre}</h3>
-            <h4>Categoría: <span> { tituloMayuscula(item.categoria) } </span></h4>
-            <h4>Precio Unitario: <span> $ { separadorMiles(item.precio) } </span></h4>
-            <div className='cart_item_detail_cant'>
-                <div className="cant_detail">
-                    <button>-</button>
-                    <p>{item.quantity}</p>
-                    <button>+</button>
-                </div>
-                <DeleteOutlinedIcon className='cant_detail_icon' onClick={ () => removeItem(item.id)}/>
-            </div>
-        </div> 
-        <h3 className='cart_item_price'>$ { separadorMiles(item.precio*item.quantity) }</h3>       
+      </div>
+      <h3 className="cart_item_price">
+        $ {separadorMiles(item.precio * item.quantity)}
+      </h3>
     </div>
-  )
+  );
 }
 
-export default CartDetail
+export default CartDetail;
