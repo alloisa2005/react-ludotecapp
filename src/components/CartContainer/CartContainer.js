2eaac0955c15cd81bf3d64b3b629d83961391b6d
Anthony Allois <anthonyallois@gmail.com>
2022-07-03 16:48:20 -0300

    Mas ajustes en el carro

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -4,6 +4,10 @@ import { CartContext } from "../../context/CartContext";
 import { separadorMiles } from "../../utilidades/Utilidades";
 import CartDetail from "../CartDetail/CartDetail";
 import "./CartContainer.css";
+import visa from '../../images/visa_logo.png'
+import mastercard from '../../images/mastercard_logo.png'
+import american from '../../images/american_logo.png'
+import diners from '../../images/diners_logo.jpg'
 
 function CartContainer() {
   const [cartList, setCartList, cantidadItems, clearCart, addCart, removeItem, montoTotalCart, iva, envio, total ] = useContext(CartContext);
@@ -29,10 +33,41 @@ function CartContainer() {
 
         <div className="cartDetail_container_right">
           <h2>Resumen del Pedido</h2>
-          <h3>SubTotal ($): <span>{separadorMiles( montoTotalCart() )}</span></h3>
-          <h3>IVA ($): <span>{separadorMiles( iva() )}</span></h3>
-          <h3>Envío ($): <span>{separadorMiles( envio() )}</span></h3>
-          <h3>TOTAL ($): <span>{separadorMiles( total() )}</span></h3>
+
+          <div className="cartDetail_container_right_section">
+            <h3>SubTotal ($):</h3>
+            <p>{separadorMiles( montoTotalCart() )}</p>  
+          </div>
+          
+          <div className="cartDetail_container_right_section">
+            <h3>IVA - 20% ($):</h3>
+            <p>{separadorMiles( iva() )}</p>  
+          </div>
+          
+          <div className="cartDetail_container_right_section">
+            <h3>Envío ($):</h3>
+            <p>{separadorMiles( envio() )}</p>  
+          </div>
+          
+          <div className="cartDetail_container_right_section_total">
+            <h3>TOTAL ($):</h3>
+            <p>{separadorMiles( total() )}</p>  
+          </div> 
+
+          <div className="cartDetail_container_right_section_logos">
+            <img src={visa} alt="Visa Logo" />
+            <img src={mastercard} alt="MasterCard Logo" />
+            <img src={american} alt="American Logo" />
+            <img src={diners} alt="Diners Logo" />
+          </div>       
+
+          <div className="botones_container">
+            <button className="botones_container_btn btn_blue">Finalizar Compra</button>
+            <Link to={'/'}>
+              <button className="botones_container_btn btn_white">Seguir Comprando</button>
+            </Link>            
+          </div>          
+
         </div>
       </div>
     </>
