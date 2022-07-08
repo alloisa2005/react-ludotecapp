88d5be6a10f78d88dc8e9f1df0b2df3c8784a6a7
Anthony Allois <anthonyallois@gmail.com>
2022-07-08 09:50:33 -0300

    Ajustes en Metodos de Firebase

--- src/firebase/firebaseFunciones.js
+++ src/firebase/firebaseFunciones.js
@@ -1,41 +1,56 @@
-
 import { db } from "./firebaseConfig";
-import { collection, query, where, getDocs, doc, getDoc } from "firebase/firestore";
+import {  collection,  query,  where,  getDocs,  doc,  getDoc,  addDoc, updateDoc  } from "firebase/firestore";
 
 export const getAllJuegos = async (tipo = undefined) => {
+  let q = null;
 
-    let q = null;
+  if (tipo === undefined) {
+    q = query(collection(db, "juegos"));
+  } else {
+    q = query(collection(db, "juegos"), where("categoria", "==", tipo));
+  }
 
-    if(tipo === undefined){
-      q = query(collection(db, "juegos"));
-    }else{
-      q = query(collection(db, "juegos"), where("categoria", "==", tipo));
-    }
+  const querySnapshot = await getDocs(q);
 
-    const querySnapshot = await getDocs(q);
+  let juegos = [];
 
-    let juegos = [];
+  querySnapshot.forEach((doc) => {
+    juegos.push({ ...doc.data(), id: doc.id });
+  });
 
-    querySnapshot.forEach((doc) => {      
-      juegos.push( {...doc.data(), id:doc.id} );     
-    });
-    
-    return juegos;
-}
-
+  return juegos;
+};
 
 export const getJuegosXId = async (id) => {
-
   const docRef = doc(db, "juegos", id);
-  const docSnap = await getDoc(docRef); 
+  const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    return {...docSnap.data(), id:id};
-  } 
+    return { ...docSnap.data(), id: id };
+  }
 
   return {};
-}
+};
 
-export const updateStockJuego = async (id, cant) => {}
+export const updateStockJuego = async (id, stock) => {
 
-export const agregarCompra = async () => {}
+  const juego = doc(db, "juegos", id);
+
+  // Set the "capital" field of the city 'DC'
+  await updateDoc(juego, {
+    stock: stock
+  });
+
+};
+
+export const agregarCompra = async (buyer, items, date, total) => {
+  // Add a new document with a generated id.
+  const docRef = await addDoc(collection(db, "compras"), {
+    buyer,
+    items,
+    date,
+    total,
+  });
+
+  return docRef.id;
+};
