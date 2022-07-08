2a78cb55eef3aabfded215743067e7b97753a93f
Anthony Allois <anthonyallois@gmail.com>
2022-07-08 14:39:16 -0300

    Mas ajustes en el cartDetail

---
+++ src/components/FormCompra/Acordeon.js
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { CartContext } from '../../context/CartContext';
+import { Divider } from '@mui/material';
+
+export default function Acordeon() {
+
+  const [cartList, setCartList, cantidadItems, clearCart, addCart, removeItem, montoTotalCart, iva, envio, total ] = useContext(CartContext);
+
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+        <Typography>Monto de Compra</Typography>
+        </AccordionSummary>        
+        <AccordionDetails>
+          <Typography>
+            
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+        <Typography>Lista de Juegos</Typography>
+        </AccordionSummary>        
+        <AccordionDetails>
+          <Typography>
+            {
+              cartList.map( juego => (
+                <div key={juego.id}>
+                  <p>{juego.nombre}</p>
+                  <hr />                  
+                </div>
+              ))
+            }
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
--- src/components/FormCompra/FormCompra.js
+++ src/components/FormCompra/FormCompra.js
@@ -1,17 +1,26 @@
-import React, {useContext} from 'react';
-import Button from '@mui/material/Button';
+import React, {useState, useContext} from 'react';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { CartContext } from '../../context/CartContext';
+import Acordeon from './Acordeon';
+
+import './FormCompra.css'
+
+const initialState = {
+  name: '',
+  address:'',
+  email: '',
+  telephone: '',
+}
 
 export default function FormCompra() {
   const [cartList, setCartList, cantidadItems, clearCart, addCart, removeItem, montoTotalCart, iva, envio, total ] = useContext(CartContext);
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [values, setValues] = useState(initialState);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -21,45 +30,82 @@ export default function FormCompra() {
     setOpen(false);
   };  
 
+  const handlerOnChange = (e) => {    
+    setValues({...values, [e.target.name]: e.target.value});    
+  }
+
+  const handleConfirmar = () => {
+    console.log(values);
+    setValues(initialState);
+    //handleClose();
+  }
+
   return (
-    <div>
+    <div className='formCompra_container'>
       <button onClick={handleClickOpen} className="botones_container_btn btn_blue">
         Finalizar Compra
       </button>
-
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Ingrese datos para la entrega</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
+      
+      <Dialog open={open} onClose={handleClose}>              
+        <DialogTitle className='formCompra_titulo'>Ingrese los datos para la entrega</DialogTitle>
+        <DialogContent>          
           <TextField
             autoFocus
             margin="dense" 
             id="name"
+            name='name'
             label="Nombre y apellido"
             type="text"
             fullWidth
+            variant="standard"        
+            onChange={handlerOnChange}    
+          />
+          <TextField
+            autoFocus
+            margin="dense" 
+            id="address"
+            name='address'
+            label="Dirección"
+            type="text"
+            fullWidth
+            variant="standard"    
+            onChange={handlerOnChange}         
+          />
+          <TextField
+            autoFocus
+            margin="dense" 
+            id="telephone"
+            name='telephone'
+            label="Teléfono"
+            type="tel"
+            fullWidth
             variant="standard"            
+            onChange={handlerOnChange} 
           />
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="email"
+            name='email'
             label="Email"
             type="email"
             fullWidth
             variant="standard"
-            helperText="Este correo será usado para enviarle los datos de la compra"
+            helperText="Este correo será usado para enviarle los datos de la compra" 
+            onChange={handlerOnChange}                        
           />
+          
         </DialogContent>
-        
+
+        {/* <DialogContent>
+          <Acordeon />
+        </DialogContent> */}
+
         <DialogActions>
           <button className='botones_container_btn btn_white' onClick={handleClose}>CANCELAR</button>
-          <button className='botones_container_btn btn_blue' onClick={handleClose}>CONFIRMAR</button>
+          <button className='botones_container_btn btn_blue' onClick={handleConfirmar}>CONFIRMAR</button>          
         </DialogActions>
-      </Dialog>
+      </Dialog>      
     </div>
   );
 }
