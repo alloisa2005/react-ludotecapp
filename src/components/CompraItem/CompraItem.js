1c21b90fe6c3c376b834aeb1eff0a05bd2462339
Anthony Allois <anthonyallois@gmail.com>
2022-07-18 10:31:22 -0300

    Ajustes varios

--- src/components/CompraItem/CompraItem.js
+++ src/components/CompraItem/CompraItem.js
@@ -9,9 +9,9 @@ function CompraItem({ compra }) {
     <div className="compra_item">
       <div className="compra_total">
         {/* <h4><span>{ transformarFecha(compra.date) }</span> - Impuestos ($): { separadorMiles( compra.total.iva )} - Envío ($): { separadorMiles( compra.total.envio )}</h4>         */}
-        <h4>
-          <span>{transformarFecha(compra.date)}</span> -{" "}
-          <span>{compra.buyer.name}</span>
+        <h4 className="compra_total_total">
+          {transformarFecha(compra.date)} -{" "}
+          {compra.buyer.name}
         </h4>
         <h4 className="compra_total_total">
           Total ($): {separadorMiles(compra.total.total)}
@@ -21,15 +21,16 @@ function CompraItem({ compra }) {
         {compra.items.map((item, index) => (
           <>
             <div key={item.id} className="compra_card">
-              <Link to={`/item/${item.id}`}>
-                {" "}
+              <Link to={`/item/${item.id}`}>                
                 <img src={item.img} alt={item.name} />{" "}
               </Link>
-              <p className="compra_card_p">{item.title}</p>
-              <p className="compra_card_p">Cantidad: {item.quantity}</p>
-              <p style={{ fontSize: "1.1rem" }}>
-                Monto ($): {separadorMiles(item.quantity * item.price)}
-              </p>
+              <div className="compra_card_detail">
+                <p className="compra_card_p">{item.title}</p>
+                <p className="compra_card_p">Cantidad: {item.quantity}</p>
+                <p className="compra_card_p">
+                  Monto ($): {separadorMiles(item.quantity * item.price)}
+                </p>
+              </div>
             </div>
             {index < compra.items.length - 1 ? <hr /> : null}
             {index === compra.items.length - 1 ? (
@@ -37,10 +38,12 @@ function CompraItem({ compra }) {
                 <hr />
                 <div className="compra_card">
                   <img src={delivery} alt="Img de delivery" />
-                  <p className="compra_card_p">Impuestos y/o Envío</p>
-                  <p style={{ fontSize: "1.1rem" }}>
-                    Monto ($): {separadorMiles(compra.total.envio + compra.total.iva)}
-                  </p>
+                  <div>
+                    <p className="compra_card_p">Impuestos y/o Envío</p>
+                    <p className="compra_card_p">
+                      Monto ($): {separadorMiles(compra.total.envio + compra.total.iva)}
+                    </p>
+                  </div>
                 </div>
               </>
             ) : null}
