1343106daef5732c65cf8516bd190ec0d7ee2711
Anthony Allois <anthonyallois@gmail.com>
2022-07-19 11:06:56 -0300

    Ajustes en JS

--- src/components/EmptyCompras/EmptyCompras.jsx
+++ src/components/EmptyCompras/EmptyCompras.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./EmptyCompras.css";
-import no_buy from "../../images/no_buy.png";
 
 function EmptyCompras() {
   return (
     <div className="compras_noItem">
-      <img src={no_buy} alt="Sin Compras Img" />
+      <img src={process.env.PUBLIC_URL+"assets/no_buy.png"}  alt="Sin Compras Img" />
       <h1>No ha realizado compras</h1>
       <Link to="/">
         <button className="itemCount_btn_cart">Inicio</button>
